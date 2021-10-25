1fd7976c2455ad59e92ebaae58d733ae3d4776cb
James Jacobson <james.jacobson@snhu.edu>
2021-10-25 19:12:09 -0400

    coded blog new page, blog new page with fourth post, fourth post page, and blog homepage with four posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 from .models import Post
+
+
 # Create your views here.
 
 
@@ -9,6 +12,13 @@ class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
 
+
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
